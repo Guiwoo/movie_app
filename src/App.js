187d5aec6bfeb21bddd8528f6fcb1201662e6e1e
Guiwoo Park <guiwoopark@Guiwooui-MacBookAir.local>
2021-05-04 16:50:56 +0900

    routing

--- src/App.js
+++ src/App.js
@@ -1,45 +1,18 @@
 import React from "react";
-import axios from "axios";
-import Movie from "./movie";
-import "./app.css";
+import {HashRouter, Route} from "react-router-dom"
+import "./App.css";
+import Navigation from "./components/Navigation";
+import about from "./routes/about"
+import home from "./routes/home"
+import detail from "./routes/detail"
 
-class App extends React.Component {
-  state={
-    isLoading: true,
-    movies: []
-  };
-
-  async componentDidMount() {
-    const {data:{data:{movies}}} = await axios.get("https://yts-proxy.nomadcoders1.now.sh/list_movies.json?sort_by=rating")
-    this.setState({movies, isLoading: false});
-  }
-
-  render(){
-    const {isLoading, movies} = this.state
-    return (
-      <section className="container">
-        {isLoading ? (
-          <div className="loader">
-            <span className="loader__text">Loading..</span>
-          </div>
-        ): (
-          <div className="movies">
-            {movies.map(movie => (
-              <Movie 
-                key={movie.id}
-                id={movie.id}
-                year={movie.year}
-                title={movie.title}
-                summary={movie.summary}
-                poster={movie.medium_cover_image}
-                genres={movie.genres}
-              />
-            ))}
-          </div>
-        )}
-      </section>
-    )
-};
+function App() {
+  return <HashRouter>
+    <Navigation/>
+    <Route path="/" exact={true} component={home}/>
+    <Route path="/about" component={about}/>
+    <Route path="/movie-detail" component={detail}/>
+  </HashRouter>
 }
 
 export default App;
